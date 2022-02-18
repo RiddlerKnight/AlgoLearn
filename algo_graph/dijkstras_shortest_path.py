7bb590f23dfc926aef3f3d7a2a2f49558f15b34d
Bawornsit.C <puksigon@gmail.com>
2022-02-18 21:47:39 +0700

    Update: dijkstra

--- algo_graph/dijkstras_shortest_path.py
+++ algo_graph/dijkstras_shortest_path.py
@@ -4,23 +4,19 @@ def FindShortPath(gp, start, end):
     # node = len(gp)
     passed_node = []    
     path = []
-
     # Find the end node is in the passed node
     while passed_node.count(end) != 1:
         new_start = 0
-        index = 0
         # Fetch weight of start node
         for node_arr in gp[start]:
             # if current node is the end node, end loop
             if start == end:
-                # path.append(node_arr)
                 break
             if len(path) == 0 or len(path) == len(passed_node):
                     path.append([])
             if (path[len(path) - 1] == [] or (node_arr[1] < path[len(path) - 1][1])) and passed_node.count(start) == 0 and passed_node.count(node_arr[0]) == 0:
                 path[len(path) - 1] = node_arr
                 new_start = node_arr[0]
-            index += 1
 
         passed_node.append(start)
         start = new_start
@@ -37,5 +33,6 @@ weight = [
     [[2,3],[1,6],[3,3],[5,1]],
     [[3,1],[4,1]],
 ]
+
 
 print(FindShortPath(weight,0,5))
